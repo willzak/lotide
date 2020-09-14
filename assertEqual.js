484ed4820fbdeb2355528bc7ec0c742eabf2650a
William <williamzak0@gmail.com>
2020-09-14 22:22:32 +0000

    created new function to return first item in an array

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
-const assetEqual = function(actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertation Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertation Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertation Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
-assetEqual("Lighthouse Labs", "Bootcamp");
-assetEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
