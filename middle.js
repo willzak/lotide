f838ddd43723c328b44264ed6874b803ada81361
William <williamzak0@gmail.com>
2020-09-16 16:01:13 +0000

    linted code

--- middle.js
+++ middle.js
@@ -36,11 +36,11 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
 
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
