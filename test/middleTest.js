236a300acd1315046c194b11dde03e32234edfee
William <williamzak0@gmail.com>
2020-09-21 20:21:58 +0000

    improved middleTest file

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,11 @@
 const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
+assertArraysEqual(middle([1]), []); // => []
+assertArraysEqual(middle([1, 2]), []); // => []
 
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
 
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
